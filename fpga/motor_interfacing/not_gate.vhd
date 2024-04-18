607891267bace61a6ee2595f62c064cb369cea8e
Simon Svendsen <simon.egeris.svendsen@gmail.com>
2024-04-18 10:45:33 +0200

    fpga motor interfacing

---
+++ fpga/motor_interfacing/not_gate.vhd
@@ -0,0 +1,45 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 02/21/2024 02:29:13 PM
+-- Design Name: 
+-- Module Name: not_gate - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity not_gate is
+    Port ( a : in STD_LOGIC;
+           o : out STD_LOGIC);
+end not_gate;
+
+architecture Behavioral of not_gate is
+
+begin
+
+    o <= not(a);
+
+end Behavioral;
